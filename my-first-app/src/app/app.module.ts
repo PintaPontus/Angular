17df7273c6ce6b45904e720373beebe4f2959a63
PintaPontus <mateo.blascoviz@gmail.com>
2022-06-10 14:05:52 +0200

    firebase setup

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-first-app/src/environments/environment.prod.ts
+++ my-first-app/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'angulartest-55eea',
+    appId: '1:892741031223:web:80fb4fbbd5eae2489176b2',
+    databaseURL: 'https://angulartest-55eea-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'angulartest-55eea.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDBqXCgKQKqx51iBGfQz0kEgeqqNvexZ1c',
+    authDomain: 'angulartest-55eea.firebaseapp.com',
+    messagingSenderId: '892741031223',
+    measurementId: 'G-SKT88X1ZTJ',
+  },
   production: true
 };
