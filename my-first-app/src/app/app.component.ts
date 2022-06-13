45736406adbd60c3a564ed525b4045c9d9df8d98
PintaPontus <mateo.blascoviz@gmail.com>
2022-06-13 21:57:23 +0200

    Firestore basics

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import {Component} from '@angular/core';
+import {Firestore, collectionData, collection, addDoc} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import {DocumentData} from "@angular/fire/compat/firestore";
 
-interface Item {
-  name: string,
-};
+class Persona {
+  constructor(private nome: string, private cognome: string) {}
+}
 
 @Component({
   selector: 'app-root',
@@ -16,15 +16,39 @@ export class AppComponent {
   title = 'my-first-app';
 
   item$: Observable<DocumentData[]>;
+  persone: Persona[] = [];
+  nomeInput: string = "Bella";
+  cognomeInput: string = "Ciao";
 
-  constructor(firestore: Firestore) {
-    const col = collection(firestore, 'items');
+  constructor(private firedb: Firestore) {
+    const col = collection(firedb, 'nomi');
     this.item$ = collectionData(col);
-    this.item$.subscribe(console.log);
+    this.item$.subscribe(
+      (data) => {
+        this.persone = [];
+        for (const d of data) {
+          this.persone.push(new Persona(d['nome'], d['cognome']));
+        }
+      }
+    );
+  }
+
+  addPersona(nome: string, cognome: string) {
+    addDoc(collection(this.firedb, "nomi"), {
+      nome: nome,
+      cognome: cognome
+    });
   }
 
   ngOnInit() {
 
   }
 
+  onAddPersona() {
+    // @ts-ignore
+    this.nomeInput = document.getElementById("nome").value;
+    // @ts-ignore
+    this.cognomeInput = document.getElementById("cognome").value;
+    this.addPersona(this.nomeInput, this.cognomeInput);
+  }
 }
